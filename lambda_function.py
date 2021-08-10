1a200ae218ae9b7cf86f637055a28303b91ccfde
Vladyslav Kriuchkovskyi <74546413+2dOverlord@users.noreply.github.com>
2021-08-10 22:13:04 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    text = "Hello World"
+    text = "Hello You"
     return {
         'statusCode': 200,
         'body': json.dumps(text)
